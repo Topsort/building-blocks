c556b04f4330a0a0bb8f7427632999dcdd931f47
César Pinto <cesar.pinto.pinto@gmail.com>
2022-08-25 19:40:53 -0400

    removed declaration file, added disable no-undef next line

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,6 @@
 import { ZodType } from "zod";
 
+// eslint-disable-next-line no-undef
 export async function api(url: string, config?: RequestInit) {
   if (!window.fetch) {
     throw "[fetch] Your browser does not support this function";
@@ -11,6 +12,7 @@ export async function api(url: string, config?: RequestInit) {
 export async function enhancedApi<T>(
   schema: ZodType<T>,
   url: string,
+  // eslint-disable-next-line no-undef
   config?: RequestInit
 ): Promise<T> {
   return schema.parse(await api(url, config));
