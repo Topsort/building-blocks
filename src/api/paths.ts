423d53d9ab13f5ed0c1f1d7c51e6b195743eb407
César Pinto <cesar.pinto.pinto@gmail.com>
2022-08-25 19:34:41 -0400

    lint path, added RequestInit declaration

--- src/api/paths.ts
+++ src/api/paths.ts
@@ -2,6 +2,5 @@
 const CENTRAL_SERVICES_URL = "http://localhost:5001/public/v1/elements-service";
 
 export default {
-  validate: (vendorId: string) =>
-    `${CENTRAL_SERVICES_URL}/vendors/${vendorId}`,
+  validate: (vendorId: string) => `${CENTRAL_SERVICES_URL}/vendors/${vendorId}`,
 };
