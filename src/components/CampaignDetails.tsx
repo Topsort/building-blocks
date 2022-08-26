7c8dba71b81f7a03e6b2dcaeeb1b6849907ce8dc
Sofía Valenzuela <sofia.valenzuela@topsort.com>
2022-08-26 12:47:29 -0400

    wip refactor

--- src/components/CampaignDetails.tsx
+++ src/components/CampaignDetails.tsx
@@ -21,9 +21,6 @@ export const CampaignDetails: FunctionalComponent<{
   );
 };
 
-const CampaignDataBox: FunctionalComponent = ({ children }) => {
-  return <div className="ts-campaign-data-box"> {children}</div>;
-};
 const CampaignTotal: FunctionalComponent<{ title: string; value: string }> = ({
   title,
   value,
@@ -40,42 +37,28 @@ const CampaignSummary: FunctionalComponent<{
   campaignDetails: Campaign;
 }> = ({ campaignDetails }) => {
   return (
-    <CampaignDataBox>
-      <div className="flex flex-col space-y-4">
-        <div className="flex flex-row space-x-4">
-          <img
-            class="border-gray rounded-xl w-17 h-17"
-            src={campaignDetails.productImageUrl}
-          />
-          <div className="text-lg" style="margin-right:1rem;">
-            {campaignDetails.name}
-          </div>
-          <div class="flex flex-col">
-            <div style="text-align: right;">{campaignDetails.budget}</div>
-            <div
-              className="text-xs text-gray"
-              style="text-align: right;white-space: nowrap;"
-            >
-              Daily Budget
-            </div>
-          </div>
+    <div className="ts-campaign-data-box space-y-4">
+      <div className="flex flex-row space-x-4">
+        <img class="ts-product-image" src={campaignDetails.productImageUrl} />
+        <div className="text-lg" style="margin-right:1rem;">
+          {campaignDetails.name}
         </div>
-        <div
-          className="flex rounded-xl p-3-5"
-          style="background: rgba(94, 59, 221, 0.03);justify-content: space-around;"
-        >
-          <CampaignTotal
-            title="Total Spend"
-            value={campaignDetails.totalSpend}
-          />
-          <CampaignTotal
-            title="Total Sales"
-            value={campaignDetails.totalSales}
-          />
-          <CampaignTotal title="ROAS" value={campaignDetails.roas} />
+        <div class="flex flex-col">
+          <div style="text-align: right;">{campaignDetails.budget}</div>
+          <div
+            className="text-xs text-gray"
+            style="text-align: right;white-space: nowrap;"
+          >
+            Daily Budget
+          </div>
         </div>
       </div>
-    </CampaignDataBox>
+      <div className="ts-purple-box">
+        <CampaignTotal title="Total Spend" value={campaignDetails.totalSpend} />
+        <CampaignTotal title="Total Sales" value={campaignDetails.totalSales} />
+        <CampaignTotal title="ROAS" value={campaignDetails.roas} />
+      </div>
+    </div>
   );
 };
 
@@ -83,37 +66,32 @@ const CampaignBudget: FunctionalComponent<{
   campaignDetails: Campaign;
 }> = ({ campaignDetails }) => {
   return (
-    <CampaignDataBox>
-      <div className="flex flex-col">
-        <div
-          className="flex flex-row"
-          style="justify-content: space-between;align-items: center;margin-bottom:0.5rem;"
+    <div className="ts-campaign-data-box">
+      <div className="title">
+        <div>Budget & Duration</div>
+        <Button
+          onClick={() => {
+            console.log("edit campaign");
+          }}
+          variant="text"
         >
-          <div className="text-lg">Budget & Duration</div>
-          <Button
-            onClick={() => {
-              console.log("click");
-            }}
-            variant="text"
-          >
-            Edit
-          </Button>
+          Edit
+        </Button>
+      </div>
+      <hr className="ts-gray-hr" />
+      <div style="margin-top:0.875rem;">
+        <div>
+          <span className="font-medium">{campaignDetails.budget} </span>over
+          <span className="font-medium"> {campaignDetails.days} </span>days.
         </div>
-        <hr className="ts-gray-hr" />
-        <div style="margin-top:0.875rem;">
-          <div>
-            <span className="font-medium">{campaignDetails.budget} </span>over
-            <span className="font-medium"> {campaignDetails.days} </span>days.
-          </div>
-          <div style="margin-top:0.25rem;" className="text-sm">
-            <span className="text-gray" style="font-style: italic;">
-              Your minimun return on ad spend is
-            </span>
-            <span className="font-medium"> {campaignDetails.minRoas}</span>.
-          </div>
+        <div style="margin-top:0.25rem;" className="text-sm">
+          <span className="text-gray" style="font-style: italic;">
+            Your minimun return on ad spend is
+          </span>
+          <span className="font-medium"> {campaignDetails.minRoas}</span>.
         </div>
       </div>
-    </CampaignDataBox>
+    </div>
   );
 };
 
@@ -121,32 +99,24 @@ const CampaignMetrics: FunctionalComponent<{
   campaignDetails: Campaign;
 }> = ({ campaignDetails }) => {
   return (
-    <CampaignDataBox>
-      <div className="flex flex-col">
-        <div
-          className="text-lg"
-          style="align-self: center;margin-bottom:0.875rem;"
-        >
-          Metrics
-        </div>
-        <hr className="ts-gray-hr" />
-        <div
-          style="margin-top:0.875rem;justify-content: space-between;"
-          className="ts-metric-grid "
-        >
-          <div className="text-gray text-sm">Impressions</div>
-          <div className="text-gray text-sm">Clicks</div>
-          <div className="text-gray text-sm">Purchases</div>
-          <div className="text-gray text-sm">Status</div>
-          <div>{campaignDetails.impressions}</div>
-          <div>{campaignDetails.clicks}</div>
-          <div>{campaignDetails.purchases}</div>
-          <div>
-            <Switch />
-          </div>
+    <div className="ts-campaign-data-box">
+      <div className="title" style="align-self: center;">
+        Metrics
+      </div>
+      <hr className="ts-gray-hr" />
+      <div className="ts-metric-grid">
+        <div className="text-gray text-sm">Impressions</div>
+        <div className="text-gray text-sm">Clicks</div>
+        <div className="text-gray text-sm">Purchases</div>
+        <div className="text-gray text-sm">Status</div>
+        <div>{campaignDetails.impressions}</div>
+        <div>{campaignDetails.clicks}</div>
+        <div>{campaignDetails.purchases}</div>
+        <div>
+          <Switch />
         </div>
       </div>
-    </CampaignDataBox>
+    </div>
   );
 };
 
