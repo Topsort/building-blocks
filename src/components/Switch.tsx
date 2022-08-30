1a9e5faeba4617256cf622150bac1b169f891b5e
Sofía Valenzuela <sofia.valenzuela@topsort.com>
2022-08-30 12:21:47 -0400

    add focus to switch

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -2,7 +2,7 @@ import { h, FunctionalComponent } from "preact";
 
 export const Switch: FunctionalComponent = () => {
   return (
-    <label class="ts-switch">
+    <label class="ts-switch" tabIndex={0}>
       <input type="checkbox" />
       <span class="ts-switch-track" />
     </label>
