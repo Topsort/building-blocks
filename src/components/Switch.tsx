676afd05caaf8efb7340e520537abed368797e51
Sofía Valenzuela <sofia.valenzuela@topsort.com>
2022-08-30 17:22:56 -0400

    pr comments

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -2,7 +2,7 @@ import { h, FunctionalComponent } from "preact";
 
 export const Switch: FunctionalComponent = () => {
   return (
-    <label class="ts-switch" tabIndex={0}>
+    <label class="ts-switch">
       <input type="checkbox" />
       <span class="ts-switch-track" />
     </label>
