39b841676efd564e91f13aa3a48a6711dd4bb1ef
Joaquin sanchez <jasanchez1@uc.cl>
2022-09-23 16:19:17 -0300

    chore: updated central services api url

--- src/api/paths.ts
+++ src/api/paths.ts
@@ -1,6 +1,9 @@
 // ToDo: somehow fetch central-services URL from build
-const CENTRAL_SERVICES_URL = "http://localhost:5001/public/v1/elements-service";
+const CENTRAL_SERVICES_PUBLIC_URL =
+  "http://localhost:5001/public/v1/elements-service";
+const CENTRAL_SERVICES_PRIVATE_URL = "http://localhost:5001/elements-service";
 
 export default {
-  validate: (vendorId: string) => `${CENTRAL_SERVICES_URL}/vendors/${vendorId}`,
+  validate: (vendorId: string) =>
+    `${CENTRAL_SERVICES_PUBLIC_URL}/auth/vendors/${vendorId}`,
 };
