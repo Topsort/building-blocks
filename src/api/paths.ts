e37857ee25c92e0288bc331f2adc799bef95a331
Joaquin sanchez <jasanchez1@uc.cl>
2022-09-26 12:09:13 -0300

    chore: central service API rename to modal API

--- src/api/paths.ts
+++ src/api/paths.ts
@@ -1,7 +1,7 @@
 // ToDo: somehow fetch central-services URL from build
 const CENTRAL_SERVICES_PUBLIC_URL =
-  "http://localhost:5001/public/v1/elements-service";
-// const CENTRAL_SERVICES_PRIVATE_URL = "http://localhost:5001/elements-service";
+  "http://localhost:5001/public/v1/modal-service";
+// const CENTRAL_SERVICES_PRIVATE_URL = "http://localhost:5001/modal-service";
 
 export default {
   validate: (vendorId: string) =>
