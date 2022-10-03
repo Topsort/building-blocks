1d2aac14ff7d16e4e55f5810555c94c6c882be43
Samet Altunsoy <samet@topsort.com>
2022-10-03 17:42:20 +0300

    create ending confirmation modal

---
+++ src/components/CampaignDetails/Ending.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@components/Button";
+import { CampaignSummary } from "@components/common";
+import { Campaign } from "@types";
+import { h, FunctionalComponent } from "preact";
+
+import { Metrics } from "./Metrics";
+import { useCampaignDetails } from "./context";
+
+export const Ending: FunctionalComponent<{
+  campaign: Campaign;
+}> = ({ campaign }) => {
+  const { dispatch } = useCampaignDetails();
+  // TODO (samet): Fetch the email from central services or somewhere else
+  const email = "samet@topsort.com";
+
+  return (
+    <div className="ts-space-y-4">
+      <CampaignSummary
+        name={campaign.name}
+        productImageUrl={campaign.productImageUrl}
+      />
+      <Metrics title="Final Metrics" campaign={campaign} />
+      <div className="ts-space-y-2-5">
+        <span className="ts-campaign-ending__email_text">
+          We will send a summary of metrics to your email ({email}).
+        </span>
+        <Button
+          color="danger"
+          fullWidth
+          variant="contained"
+          onClick={() => dispatch({ type: "end campaign button clicked" })}
+        >
+          End campaign
+        </Button>
+        <Button
+          fullWidth
+          variant="text"
+          onClick={() => dispatch({ type: "end campaign back button clicked" })}
+        >
+          Go back
+        </Button>
+      </div>
+    </div>
+  );
+};
