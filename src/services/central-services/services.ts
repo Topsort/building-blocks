3062b318288744cecb99b438b24428529e70b3cc
Sofía <sofia.valenzuela@topsort.com>
2023-08-01 10:11:38 -0400

    get shop campaign lint

--- src/services/central-services/services.ts
+++ src/services/central-services/services.ts
@@ -315,15 +315,15 @@ async function getShopCampaign(
   authToken: string,
   vendorId: string
 ): Promise<Campaign | null> {
-  return Promise.resolve(null);
-  // return await api(
-  //   schemas.campaignSchema,
-  //   paths.campaignByShop(centralServicesUrl, vendorId),
-  //   {
-  //     method: "GET",
-  //     headers: getHeaders(authToken),
-  //   }
-  // );
+  // TODO (sofia):change path when we integrate with cs
+  return await api(
+    schemas.campaignSchema,
+    paths.campaignByShop(centralServicesUrl, vendorId),
+    {
+      method: "GET",
+      headers: getHeaders(authToken),
+    }
+  );
 }
 
 export const services: Services = {
