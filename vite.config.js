10bc666fcf4737c28b43a789444a27fbac3c482b
Johan Bergström <bugs@bergstroem.nu>
2023-08-09 11:51:17 -0400

    feat: switch webpack for vite
    This removes webpack/babel in favor of vite. Build times are about 80% faster on my machine.
    It also removes some of the (albeit fancier) dynamic injection of scripts since we don't really need it.

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import preact from "@preact/preset-vite";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import EnvironmentPlugin from "vite-plugin-environment";
+
+export default defineConfig({
+  root: ".",
+  resolve: {
+    alias: {
+      "@api": resolve(__dirname, "src/api/"),
+      "@components": resolve(__dirname, "src/components/"),
+      "@constants": resolve(__dirname, "src/constants"),
+      "@context": resolve(__dirname, "src/context"),
+      "@hooks": resolve(__dirname, "src/hooks/"),
+      "@services": resolve(__dirname, "src/services/"),
+      "@state": resolve(__dirname, "src/state"),
+      "@types": resolve(__dirname, "src/types"),
+      "@utils": resolve(__dirname, "src/utils/"),
+    },
+  },
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/index.tsx"),
+      name: "building-blocks",
+      // the proper extensions will be added
+      fileName: (format) =>
+        `index.${format === "es" ? "mjs" : format === "umd" ? "js" : "js"}`,
+      formats: ["es", "umd"],
+    },
+    // Relative to the root
+    outDir: "dist",
+  },
+  plugins: [preact(), EnvironmentPlugin({ NODE_ENV: "production" },
+  )],
+});
