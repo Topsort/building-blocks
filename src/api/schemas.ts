71f3e87f48ac5dba390ea6897ae17a445b53ffa8
Sofía <sofia.valenzuela@topsort.com>
2023-08-21 17:42:55 -0400

    fix: type error

--- src/api/schemas.ts
+++ src/api/schemas.ts
@@ -23,15 +23,15 @@ export const campaignIdsByProductIdSchema = z.record(
 
 export const reportingApiModels = (function () {
   const events = z.object({
-    total: z.bigint(),
-    charged: z.bigint(),
-    adSpent: z.bigint(),
+    total: z.number(),
+    charged: z.number(),
+    adSpent: z.number(),
   });
   const purchases = z.object({
-    amount: z.bigint(),
-    count: z.bigint(),
-    quantity: z.bigint(),
-    countByProduct: z.record(z.string(), z.bigint()),
+    amount: z.number(),
+    count: z.number(),
+    quantity: z.number(),
+    countByProduct: z.record(z.string(), z.number()).nullable(),
   });
   const reportData = z.object({
     impressions: events,
@@ -56,10 +56,12 @@ export const reportingApiModels = (function () {
 export const campaignBaseSchema = z.object({
   campaignId: z.string().uuid(),
   name: z.string(),
-  budget: z.object({
-    amount: z.number(),
-    type: z.enum(["daily", "weekly", "monthly"]),
-  }),
+  budget: z
+    .object({
+      amount: z.number(),
+      type: z.enum(["daily", "weekly", "monthly"]),
+    })
+    .nullable(),
   startDate: z.string(),
   endDate: z.string(),
 });
