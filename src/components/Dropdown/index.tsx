40d64a5da9d19bc29593285c007e791d6493dfad
sofiavalenzuelapugh <sofia.valenzuela@topsort.com>
2023-08-22 14:59:34 -0400

    feat: add dropdown component (#153)
    Add dropdown component. Needed for selecting dates on the graph
    
    
    ![image](https://github.com/Topsort/building-blocks/assets/95320456/2756830b-01fb-4ff6-a70b-0d91495e8243)

---
+++ src/components/Dropdown/index.tsx
@@ -0,0 +1,90 @@
+import { Button } from "@components/Button";
+import classnames from "classnames";
+import { useState } from "preact/hooks";
+
+import { ClickAwayListener } from "../ClickAwayListener";
+import { Icon } from "../Icon";
+import "./style.css";
+
+type DropdownOption<T> = {
+  value: T;
+  label: string;
+};
+
+type DropdownProps<T> = {
+  onOptionSelected: (option: DropdownOption<T>) => void;
+  options: DropdownOption<T>[];
+  defaultOption?: DropdownOption<T>;
+  disabled?: boolean;
+  dropdownClassname?: string;
+  optionsClassname?: string;
+  buttonClassname?: string;
+};
+
+export function Dropdown<T>({
+  onOptionSelected,
+  disabled,
+  optionsClassname,
+  options,
+  defaultOption,
+  buttonClassname,
+}: DropdownProps<T>) {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<DropdownOption<T>>(
+    defaultOption || options[0]
+  );
+
+  const onOptionClicked = (optionClicked: DropdownOption<T>) => {
+    setSelectedOption(optionClicked);
+    onOptionSelected(optionClicked);
+    setDropdownOpen(false);
+  };
+  return (
+    <ClickAwayListener
+      onClickAway={() => {
+        setDropdownOpen(false);
+      }}
+    >
+      <div id="ts-dropdown-container" className="ts-dropdown-container">
+        <Button
+          id="ts-dropdown-button"
+          variant="outlined"
+          onClick={() => {
+            if (!disabled) {
+              setDropdownOpen(!dropdownOpen);
+            }
+          }}
+          className={classnames("ts-space-x-1", buttonClassname)}
+        >
+          <span>{selectedOption.label}</span>
+          <Icon
+            name="arrow-up-1-linear"
+            className="ts-rotate-180"
+            width="15"
+            height="15"
+          />
+        </Button>
+        <div
+          id="ts-dropdown-menu"
+          className={classnames("ts-dropdown-menu", {
+            "ts-hidden": !dropdownOpen,
+          })}
+        >
+          {options.map((option, index) => {
+            return (
+              <Button
+                key={index}
+                variant="text"
+                onClick={() => onOptionClicked(option)}
+                selected={selectedOption === option}
+                className={optionsClassname}
+              >
+                {option.label || option.value}{" "}
+              </Button>
+            );
+          })}
+        </div>
+      </div>
+    </ClickAwayListener>
+  );
+}
